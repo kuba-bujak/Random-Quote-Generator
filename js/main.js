229fd38037e9c8f175f4f480a3ef7ca7e50b17cf
Jakub Bujak <kuba.bujak0@wp.pl>
2022-10-18 22:52:18 +0200

    Create Project

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const quoteText = document.querySelector('#text');
+const quoteAuthor = document.querySelector('#author');
+const newQuoteBtn = document.querySelector('#new-quote');
+
+
+createQuote()
+	.catch(err => console.log(err));
+
+async function createQuote() {
+	const postStream = await fetch('../quotes.json');
+	const colorStream = await fetch('../colors.json');
+	const colors = await colorStream.json()
+	const posts = await postStream.json();
+
+	let i = Math.floor(Math.random() * posts.length);
+	let x = Math.floor(Math.random() * colors.length);
+
+	const text = posts[i].quote;
+	const author = posts[i].author;
+	const fontColor = colors[x].color;
+
+	quoteText.innerText = text;
+	quoteAuthor.innerText = author;
+	document.body.style.backgroundColor = fontColor;
+	document.body.style.color = fontColor;
+	document.querySelectorAll('.button').forEach(button => {
+		button.style.backgroundColor = fontColor;
+	})
+}
+
+newQuoteBtn.addEventListener('click', createQuote);
